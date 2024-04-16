0a4052496ac40ae18aa4e220e94e08dbc44cc85b
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-04-16 14:51:18 +0200

    [Inserita logica password con ciclo for ]

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+if (isset($_GET['length']) && is_numeric($_GET['lenght'])) {
+
+  $lenght = $_GET['length'];
+
+  //Definisco i caratteri delle passowrd
+  $characetrs = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+';
+
+  $password = '';
+
+  //In questo for genero la password con i caratteri definiti sopra nella variabile $characters
+
+  for ($i = 0; $i < $lenght; $i++) {
+    $password .= $characters[rand(0, strlen($characters) - 1)];
+  }
+
+  echo "La tua password è: " . $password;
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +30,6 @@
 </head>
 
 <body>
-
 </body>
 
 </html>
